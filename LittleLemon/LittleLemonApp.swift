1ea1ef6464cb62012d8baa0f8dfe0cae8d14d47d
Albert Tang <tangalbert919@yahoo.com>
2024-10-01 17:14:18 -0500

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Albert Tang on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
